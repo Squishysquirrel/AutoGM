dc265b561744821809f96b16cd21235616564e66
Squishysquirrel <Squishysquirrel9@gmail.com>
2018-02-27 19:52:17 -0500

    Made new branch for setting up configuration files

--- src/bot/blackhole/AutoGM.java
+++ src/bot/blackhole/AutoGM.java
@@ -7,11 +7,23 @@ import sx.blah.discord.util.DiscordException;
 public class AutoGM {
 
 	public static IDiscordClient client;
-	private final static String TOKEN = "MTgyMjg4MTA2MzgzMzQzNjE3.Ch1Ihw.xLlqB5RN7dwTvFtIplvxr5gHN6Q";
+	private static String TOKEN;
+
+	private static void loadConfiguration() {
+
+		final TOKEN = ; // read from file
+		// do config stuff here
+	}
 
 	public static void main(String[] args) throws DiscordException {
+		loadConfiguration();
 		client = new ClientBuilder().withToken(TOKEN).login();
 		client.getDispatcher().registerListener(new CommandListener());
 		client.getDispatcher().registerListener(new CommandExecutor(client));
 	}
+}
+
+int getYearModel(int model)
+{
+	return model;
 }
