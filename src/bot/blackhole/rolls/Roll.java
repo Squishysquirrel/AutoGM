08a2ec049da1243c1e75e93c92222765a368b9e2
Squishysquirrel <squishysquirrel9@gmail.com>
2018-03-08 17:13:23 -0500

    Update Roll.java

--- src/bot/blackhole/rolls/Roll.java
+++ src/bot/blackhole/rolls/Roll.java
@@ -5,5 +5,5 @@ public interface Roll {
     protected final int rollAmount; // This is how many rolls one makes ex: xd6
     protected final int diceSize; // This is how many sides a die has ex: 1dx
 
-    List<Integer> makeRoll();
+    protected List<Integer> makeRoll();
 }
